8fb061e71477719465b5d14552a8ae94212e56d8
Cole Campbell <ccampbell211@my.bcit.ca>
2026-03-31 09:26:02 -0700

    changed gradle and kotlin versions to work with room

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,15 +1,13 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.devtools.ksp") version "2.0.21-1.0.25"
+    alias(libs.plugins.kotlin.android)
 }
 
 android {
     namespace = "com.bcit.myminiandroidapp"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.bcit.myminiandroidapp"
@@ -37,9 +35,15 @@ android {
     buildFeatures {
         compose = true
     }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
 }
 
 dependencies {
+    implementation("androidx.room:room-ktx:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
